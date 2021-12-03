140bcb74b1a7c4dea5c4c8c7436b9c2ae53a03a9
mshnschnko <misha321654987i@gmail.com>
2021-12-03 10:59:35 +0300

    edited

--- research/research.py
+++ research/research.py
@@ -14,8 +14,8 @@ def execute(connection, mycursor, execute_quer):
     connection.commit()
 
 def DB_Init(con):
-    query = """CREATE TABLE IF NOT EXISTS maintable 
-    (maintable_id INT PRIMARY KEY AUTO_INCREMENT, 
+    query = """CREATE TABLE IF NOT EXISTS research 
+    (research_id INT PRIMARY KEY AUTO_INCREMENT, 
     data1 NVARCHAR(64) NOT NULL,
     data2 INT)"""
     mycursor = con.cursor()
@@ -23,7 +23,7 @@ def DB_Init(con):
     con.commit()
 
 def DB_Drop(con):
-    query = "DROP TABLE IF EXISTS DB_for_research.maintable"
+    query = "DROP TABLE IF EXISTS DB_for_research.research"
     mycursor = con.cursor()
     mycursor.execute(query)
     con.commit()
@@ -31,10 +31,10 @@ def DB_Drop(con):
 def ReturnToTheStartingPoint(con, count, text):
     DB_Drop(con)
     DB_Init(con)
-    db_initial_insert_rows(con, count, text)
+    DB_InitialInsertRows(con, count, text)
 
-def db_initial_insert_rows(con, count, text):
-    query = "INSERT INTO maintable (data1, data2) VALUES "
+def DB_InitialInsertRows(con, count, text):
+    query = "INSERT INTO research (data1, data2) VALUES "
     for i in range(count - 1):
         split_begin = random.randint(0, int(len(text) / 2))
         split_end = random.randint(int(len(text) / 2), len(text) - 1)
@@ -48,29 +48,29 @@ def db_initial_insert_rows(con, count, text):
     con.commit()
     return (end - start) / 1e6
 
-def db_search_key_column(con, key):
+def SearchKeyColumn(con, key):
     mycursor = con.cursor()
     start = time.perf_counter_ns()
     for i in range(1000):
-        query = f"SELECT * FROM maintable WHERE maintable_id = {random.randint(1, key)};"
+        query = f"SELECT * FROM research WHERE research_id = {random.randint(1, key)};"
         mycursor.execute(query)
         #mycursor.fetchall()
     end = time.perf_counter_ns()
     con.commit()
     return (end - start) / (1e6 * 1000)
 
-def db_search_nonkey_column(con, num):
+def SearchNonKeyColumn(con, num):
     mycursor = con.cursor()
     start = time.perf_counter_ns()
     for i in range(100):
-        query = f"SELECT * FROM maintable WHERE data2 = {i + num};"
+        query = f"SELECT * FROM research WHERE data2 = {i + num};"
         mycursor.execute(query)
     end = time.perf_counter_ns()
     con.commit()
     return (end - start) / (1e6 * 100)
 
-def db_search_by_mask(con, mask):
-    query = f"SELECT * FROM maintable WHERE data1 LIKE '{mask}';"
+def SearchByMask(con, mask):
+    query = f"SELECT * FROM research WHERE data1 LIKE '{mask}';"
     mycursor = con.cursor()
     start = time.perf_counter_ns()
     mycursor.execute(query)
@@ -78,8 +78,8 @@ def db_search_by_mask(con, mask):
     con.commit()
     return (end - start) / 1e6
 
-def db_insert_row(con, text, num):
-    query = f"INSERT INTO maintable (data1, data2) VALUES ('{text}', {num});"
+def InsertRow(con, text, num):
+    query = f"INSERT INTO research (data1, data2) VALUES ('{text}', {num});"
     mycursor = con.cursor()
     start = time.perf_counter_ns()
     mycursor.execute(query)
@@ -87,8 +87,8 @@ def db_insert_row(con, text, num):
     con.commit()
     return (end - start) / 1e6
 
-def db_insert_some_rows(con, text, num, count):
-    query = "INSERT INTO maintable (data1, data2) VALUES "
+def InsertSomeRows(con, text, num, count):
+    query = "INSERT INTO research (data1, data2) VALUES "
     for i in range(count - 1):
         query += f"('{text}', {num}), "
     query += f"('{text}', {num});"
@@ -99,8 +99,8 @@ def db_insert_some_rows(con, text, num, count):
     con.commit()
     return (end - start) / 1e6
 
-def db_update_by_key_column(con, key, text, num):
-    query = f"UPDATE maintable SET data1 = '{text}', data2 = {num} WHERE maintable_id = {key};"
+def UpdateByKeyColumn(con, key, text, num):
+    query = f"UPDATE research SET data1 = '{text}', data2 = {num} WHERE research_id = {key};"
     mycursor = con.cursor()
     start = time.perf_counter_ns()
     mycursor.execute(query)
@@ -108,8 +108,8 @@ def db_update_by_key_column(con, key, text, num):
     con.commit()
     return (end - start) / 1e6
 
-def db_update_by_num_column(con, text, num):
-    query = f"UPDATE maintable SET data1 = '{text}' WHERE data2 = {num};"
+def UpdateByNumColumn(con, text, num):
+    query = f"UPDATE research SET data1 = '{text}' WHERE data2 = {num};"
     mycursor = con.cursor()
     start = time.perf_counter_ns()
     mycursor.execute(query)
@@ -117,8 +117,8 @@ def db_update_by_num_column(con, text, num):
     con.commit()
     return (end - start) / 1e6
 
-def db_delete_by_key_column(con, key):
-    query = f"DELETE FROM maintable WHERE maintable_id = {key};"
+def DeleteByKeyColumn(con, key):
+    query = f"DELETE FROM research WHERE research_id = {key};"
     mycursor = con.cursor()
     start = time.perf_counter_ns()
     mycursor.execute(query)
@@ -126,8 +126,8 @@ def db_delete_by_key_column(con, key):
     con.commit()
     return (end - start) / 1e6
 
-def db_delete_by_num_column(con, num):
-    query = f"DELETE FROM maintable WHERE data2 = {num};"
+def DeleteByNumColumn(con, num):
+    query = f"DELETE FROM research WHERE data2 = {num};"
     mycursor = con.cursor()
     start = time.perf_counter_ns()
     mycursor.execute(query)
@@ -135,8 +135,8 @@ def db_delete_by_num_column(con, num):
     con.commit()
     return (end - start) / 1e6
 
-def db_delete_some_rows(con, count):
-    query = f"DELETE FROM maintable LIMIT {count};"
+def DeleteSomeRows(con, count):
+    query = f"DELETE FROM research LIMIT {count};"
     mycursor = con.cursor()
     start = time.perf_counter_ns()
     mycursor.execute(query)
@@ -144,8 +144,8 @@ def db_delete_some_rows(con, count):
     con.commit()
     return (end - start) / 1e6
 
-def db_compress(con):
-    query = "ALTER TABLE maintable ROW_FORMAT=COMPRESSED;"
+def Compress(con):
+    query = "ALTER TABLE research ROW_FORMAT=COMPRESSED;"
     mycursor = con.cursor()
     start = time.perf_counter_ns()
     mycursor.execute(query)
@@ -162,28 +162,28 @@ if __name__ == '__main__':
     rowGroupCount = 200
     for rowCount in [1000, 10000, 100000]:
         ReturnToTheStartingPoint(con, rowCount, text)
-        print('Search key column {} rows: {:.4f} ms'.format(rowCount, db_search_key_column(con, rowCount)))
+        print('Search key column {} rows: {:.4f} ms'.format(rowCount, SearchKeyColumn(con, rowCount)))
         ReturnToTheStartingPoint(con, rowCount, text)
-        print('Search non-key column {} rows: {:.4f} ms'.format(rowCount, db_search_nonkey_column(con, number)))
+        print('Search non-key column {} rows: {:.4f} ms'.format(rowCount, SearchNonKeyColumn(con, number)))
         ReturnToTheStartingPoint(con, rowCount, text)
-        print('Search by mask {} rows: {:.4f} ms'.format(rowCount, db_search_by_mask(con, mask)))
+        print('Search by mask {} rows: {:.4f} ms'.format(rowCount, SearchByMask(con, mask)))
         ReturnToTheStartingPoint(con, rowCount, text)
-        print('Insert one row {} rows: {:.4f} ms'.format(rowCount, db_insert_row(con, text, number)))
+        print('Insert one row {} rows: {:.4f} ms'.format(rowCount, InsertRow(con, text, number)))
         ReturnToTheStartingPoint(con, rowCount, text)
-        print('Insert group of rows {} rows: {:.4f} ms'.format(rowCount, db_insert_some_rows(con, text, number, rowGroupCount)))
+        print('Insert group of rows {} rows: {:.4f} ms'.format(rowCount, InsertSomeRows(con, text, number, rowGroupCount)))
         ReturnToTheStartingPoint(con, rowCount, text)
-        print('Update key column {} rows: {:.4f} ms'.format(rowCount, db_update_by_key_column(con, key, text, number)))
+        print('Update key column {} rows: {:.4f} ms'.format(rowCount, UpdateByKeyColumn(con, key, text, number)))
         ReturnToTheStartingPoint(con, rowCount, text)
-        print('Update num (non-key) column {} rows: {:.4f} ms'.format(rowCount, db_update_by_num_column(con, text, number)))
-        print('Delete key column {} rows: {:.4f} ms'.format(rowCount, db_delete_by_key_column(con, key)))
+        print('Update num (non-key) column {} rows: {:.4f} ms'.format(rowCount, UpdateByNumColumn(con, text, number)))
+        print('Delete key column {} rows: {:.4f} ms'.format(rowCount, DeleteByKeyColumn(con, key)))
         ReturnToTheStartingPoint(con, rowCount, text)
-        print('Delete num (non-key) column {} rows: {:.4f} ms'.format(rowCount, db_delete_by_num_column(con, number)))
+        print('Delete num (non-key) column {} rows: {:.4f} ms'.format(rowCount, DeleteByNumColumn(con, number)))
         ReturnToTheStartingPoint(con, rowCount, text)
-        print('Delete group of rows {} rows: {:.4f} ms'.format(rowGroupCount, db_delete_some_rows(con, rowGroupCount)))
+        print('Delete group of rows {} rows: {:.4f} ms'.format(rowGroupCount, DeleteSomeRows(con, rowGroupCount)))
         ReturnToTheStartingPoint(con, rowCount, text)
-        db_delete_some_rows(con, rowGroupCount)
-        print('Compress db after 200 rows deletion {} rows: {} ms'.format(rowCount, db_compress(con)))
+        DeleteSomeRows(con, rowGroupCount)
+        print('Compress db after 200 rows deletion {} rows: {} ms'.format(rowCount, Compress(con)))
         ReturnToTheStartingPoint(con, rowGroupCount, text)
-        print('Compress db with 200 rows left {} rows: {} ms'.format(rowCount, db_compress(con)))
+        print('Compress db with 200 rows left {} rows: {} ms'.format(rowCount, Compress(con)))
         ReturnToTheStartingPoint(con, rowCount, text)
         print("\n\n")
